3940dba851dd78abb4ba374fc2a1facb61a38d3b
Seth Allgire <seth.allgire@gmail.com>
2021-12-17 11:23:41 -0600

    adds module 2

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,9 +8,13 @@ class Quiz extends Component {
     this.state = { quiz_position: 1 };
   }
   render() {
-    <div>
-      <div className="QuizQuestion"></div>
-    </div>;
+    return (
+      <div>
+        <div className="QuizQuestion">
+          {quizData.quiz_questions[0].instruction_text}
+        </div>
+      </div>
+    );
   }
 }
 

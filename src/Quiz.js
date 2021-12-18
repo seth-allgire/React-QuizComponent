7985f72b77579fd8fe4b1ef95a1011239be14f86
Seth Allgire <seth.allgire@gmail.com>
2021-12-18 10:31:35 -0600

    adds solution for module 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,13 +14,18 @@ class Quiz extends Component {
       return { quiz_position: state.quiz_position + 1 };
     });
   }
+  handleResetClick() {
+    this.setState(() => {
+      return { quiz_position: 1 };
+    });
+  }
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
         ) : (
           <QuizQuestion
             quiz_question={
